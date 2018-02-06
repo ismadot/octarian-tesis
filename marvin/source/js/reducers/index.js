7fe8d2ddc762de429ad048bcc083820014781868
erifranck <erifranckjose@gmail.com>
2018-02-05 20:56:15 -0500

    install redux persist

--- marvin/source/js/reducers/index.js
+++ marvin/source/js/reducers/index.js
@@ -5,12 +5,27 @@ import projects from 'reducers/projects';
 import auth from 'reducers/auth';
 import info from 'reducers/info';
 import app from 'reducers/app';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
-export default combineReducers({
+// redux persist is a library for data persist in store
+const configRoot = {
+  key: 'root',
+  storage
+}
+
+const configAuth = {
+  key: 'auth',
+  storage
+}
+
+const root = combineReducers({
   app,
   info,
-  auth,
+  auth: persistReducer( configAuth, auth ),
   people,
   projects,
-  categorys,
+  categorys
 });
+
+export default persistReducer( configRoot, root)
